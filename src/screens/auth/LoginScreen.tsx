d157cb7e17ad623034a0bf6dc1c66415dc83d4dc
Kim Min Gyu <ysjk2003@gmail.com>
2023-10-23 08:46:11 +0900

    add: create web-client files

---
+++ src/screens/auth/LoginScreen.tsx
@@ -0,0 +1,65 @@
+import React, {useRef} from 'react';
+import {SafeAreaView, StyleSheet, TextInput, View} from 'react-native';
+import InputField from '../../components/InputField';
+import CustomButton from '../../components/CustomButton';
+import useForm from '../../hooks/useForm';
+import {validateLogin} from '../../utils';
+
+function LoginScreen() {
+  const passwordRef = useRef<TextInput | null>(null);
+  const login = useForm({
+    initialValue: {email: '', password: ''},
+    validate: validateLogin,
+  });
+
+  const handleSubmit = () => {
+    console.log('login.values', login.values);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          autoFocus
+          placeholder="이메일"
+          error={login.errors.email}
+          touched={login.touched.email}
+          inputMode="email"
+          returnKeyType="next"
+          submitBehavior="submit"
+          onSubmitEditing={() => passwordRef.current?.focus()}
+          {...login.getTextInputProps('email')}
+        />
+        <InputField
+          ref={passwordRef}
+          placeholder="비밀번호"
+          error={login.errors.password}
+          touched={login.touched.password}
+          secureTextEntry
+          returnKeyType="join"
+          onSubmitEditing={handleSubmit}
+          {...login.getTextInputProps('password')}
+        />
+      </View>
+      <CustomButton
+        label="로그인"
+        variant="filled"
+        size="large"
+        onPress={handleSubmit}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+    marginBottom: 30,
+  },
+});
+
+export default LoginScreen;
