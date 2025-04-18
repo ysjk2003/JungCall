78dc0fac055e0560cbc4fd1c01ff167120f5f162
Kim Min Gyu <ysjk2003@gmail.com>
2025-04-18 10:03:39 +0900

    fix: password validate reg

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -5,15 +5,19 @@ type UserInfomation = {
 
 function validateUser(values: UserInfomation) {
   const errors = {
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   };
 
   if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
-    errors.email = '올바른 이메일 형식이 아닙니다.';
+    errors.email = "올바른 이메일 형식이 아닙니다.";
   }
-  if (!(values.password.length >= 8 && values.password.length <= 20)) {
-    errors.password = '비밀번호는 8~20자 사이로 입력해주세요.';
+  if (
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,20}$/.test(
+      values.password
+    )
+  ) {
+    errors.password = "숫자와 특수문자를 포함하여 8~20자 사이로 입력해주세요.";
   }
 
   return errors;
@@ -23,15 +27,15 @@ function validateLogin(values: UserInfomation) {
   return validateUser(values);
 }
 
-function validateSignup(values: UserInfomation & {passwordConfirm: string}) {
+function validateSignup(values: UserInfomation & { passwordConfirm: string }) {
   const errors = validateUser(values);
-  const signupErrors = {...errors, passwordConfirm: ''};
+  const signupErrors = { ...errors, passwordConfirm: "" };
 
   if (values.password !== values.passwordConfirm) {
-    signupErrors.passwordConfirm = '비밀번호가 일치하지않습니다.';
+    signupErrors.passwordConfirm = "비밀번호가 일치하지않습니다.";
   }
 
   return signupErrors;
 }
 
-export {validateLogin, validateSignup};
+export { validateLogin, validateSignup };
