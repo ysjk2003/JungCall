0f7b0865b29d77aefd0cff25b1975833d174dc8f
Kim Min Gyu <ysjk2003@gmail.com>
2025-04-29 22:56:05 +0900

    fix: error handling

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Platform } from "react-native";
+import { Alert, Platform } from "react-native";
 
 const axiosInstance = axios.create({
   baseURL:
@@ -7,6 +7,18 @@ const axiosInstance = axios.create({
       ? "http://10.0.2.2:3030"
       : "http://localhost:3030",
   withCredentials: true,
+});
+
+axiosInstance.interceptors.response.use(undefined, (error) => {
+  if (error.response && error.response.status === 401) {
+    Alert.alert("인증되지 않음", "인증되지 않은 사용자입니다.");
+  } else if (error.response && error.response.status === 404) {
+    Alert.alert("요청이 잘못됨", "요청형식이 잘못되었거나 찾을 수 없습니다.");
+  } else if (error.response && error.response.status === 500) {
+    Alert.alert("서버 이상", "잠시 후 다시 시도해주세요.");
+  }
+
+  return Promise.reject(error);
 });
 
 export default axiosInstance;
